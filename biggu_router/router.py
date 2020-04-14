8b3704d5b381182b9786030e15cce63ffc2fa62a
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-04-13 19:22:27 -0500

    Add Group decorator to router
    add unit test for route groups

--- biggu_router/router.py
+++ biggu_router/router.py
@@ -1,7 +1,7 @@
 class Router:
     def __init__(self, app):
         self.app = app
-        self.group_stack = {}
+        self.group_stack = []
         self.routes = {}
         self.named_routes = {}
 
@@ -11,24 +11,22 @@ class Router:
                 attributes['middleware'] = attributes['middleware'].split('|')
             self.update_group_stack(attributes)
             func(self)
-            # TODO: POP last item in group stack
+            self.group_stack.pop()
         return wrapper
 
     def update_group_stack(self, attributes):
-        if not self.group_stack:
+        if self.has_group_stack():
             attributes = self.merge_with_last_group(attributes)
-        self.group_stack = attributes
+        self.group_stack.append(attributes)
 
     def merge_with_last_group(self, new):
-        items = list(self.group_stack.items())
-        key, values = items.pop() if items else [None, {}]
+        items = self.group_stack
+        values = items[-1] if items else {}
         return self.merge_group(new, values)
 
     def merge_group(self, new, old):
-        # TODO: add namespace build
-        new['namespace'] = ''
-        # TODO: add prefix build
-        new['prefix'] = ''
+        new['namespace'] = self.format_uses_prefix(new, old)
+        new['prefix'] = self.format_group_prefix(new, old)
         if 'domain' in new:
             old.pop('domain', None)
 
@@ -39,9 +37,33 @@ class Router:
         if 'suffix' in old and 'suffix' not in new:
             new['suffix'] = old['suffix']
 
-        filtered = filter(lambda item: item[0] in ['namespace', 'prefix', 'as', 'suffix'], old.items())
+        filtered = filter(lambda item: item[0] not in ['namespace', 'prefix', 'as', 'suffix'], old.items())
         result = { key : value for key,value in filtered }
         return self.merge_recursive(result, new)
+
+    @staticmethod
+    def format_uses_prefix(new, old):
+        if 'namespace' in new:
+            if 'namespace' in old and new['namespace'].find('.') != -1:
+                return old['namespace'].strip('.') + '.' + new['namespace'].strip('.')
+            else:
+                return  new['namespace'].strip('.')
+        return old['namespace'] if 'namespace' in old else None
+
+    @staticmethod
+    def format_group_prefix(new, old):
+        if 'prefix' in old:
+            old_prefix = old['prefix']
+        else:
+            old_prefix = None
+
+        if 'prefix' in new:
+            if old_prefix:
+                return old_prefix.strip('/') + '/' + new['prefix'].strip('/')
+            else:
+                return '/' + new['prefix'].strip('/')
+
+        return old_prefix
 
     def merge_recursive(self, target, source):
         for key, value in source.items():
@@ -53,6 +75,19 @@ class Router:
         return target
 
     def add_route(self, method, uri, action):
+        action = self.parse_action(action)
+        attributes = None
+        if self.has_group_stack():
+            attributes = self.merge_with_last_group({})
+
+        if type(attributes) == dict:
+            if 'prefix' in attributes and attributes['prefix']:
+                uri = attributes['prefix'].strip('/') + '/' + uri.strip('/')
+
+            if 'suffix' in attributes and attributes['suffix']:
+                uri = uri.strip('/') + '/' + attributes['suffix'].strip('/')
+
+            action = self.merge_group_attributes(action, attributes)
 
         uri = '/' + uri.strip('/')
 
@@ -64,6 +99,63 @@ class Router:
                 self.routes[verb + uri] = {'method': verb, 'uri': uri, 'action': action}
         else:
             self.routes[method + uri] = { 'method': method,  'uri': uri, 'action': action }
+
+    @staticmethod
+    def parse_action(action):
+        if type(action) == str:
+            return {'uses': action}
+        elif type(action) != dict:
+            return {action}
+
+        if 'middleware' in action and type(action['middleware']) == str:
+            action['middleware'] = action['middleware'].split('|')
+
+        return action
+
+    # Merge current route attributes (namespace, middleware and alias) with group attributes
+    def merge_group_attributes(self, action, attributes):
+        namespace = attributes['namespace'] if 'namespace' in attributes else None
+        middleware = attributes['middleware'] if 'middleware' in attributes else None
+        alias = attributes['as'] if 'as' in attributes else None
+
+        grouped_aliases = self.merge_alias_group(action, alias)
+        grouped_middleware = self.merge_middleware_group(grouped_aliases, middleware)
+        return self.merge_namespace_group(grouped_middleware, namespace)
+
+    # If group has middleware list then merge current route middleware list with previous group list
+    @staticmethod
+    def merge_middleware_group(action, middleware = None):
+        if type(middleware) == list:
+            if 'middleware' in action:
+                # spread the group middleware list in to a temporal list
+                list_middleware = [*middleware]
+                # combine unique values keeping order (other merge techniques not do it)
+                [list_middleware.append(item) for item in action['middleware'] if item not in list_middleware]
+                action['middleware'] = list_middleware
+            else:
+                action['middleware'] = middleware
+        return action
+
+    @staticmethod
+    def merge_alias_group(action, alias = None):
+        if alias:
+            if 'as' in action:
+                action['as'] = alias + '.' + action['as']
+            else:
+                action['as'] = alias
+        return action
+
+    def merge_namespace_group(self, action, namespace):
+        if namespace and 'uses' in action:
+            action['uses'] = self.prepend_group_namespace(action['uses'], namespace)
+        return action
+
+    @staticmethod
+    def prepend_group_namespace(class_name, namespace):
+        return namespace + '.' + class_name if namespace and class_name.find('.') != 0 else class_name
+
+    def has_group_stack(self):
+        return len(self.group_stack) != 0
 
     def head(self, uri, action):
         self.add_route('HEAD', uri, action)
--- test/test_router.py
+++ test/test_router.py
@@ -9,7 +9,13 @@ class RouterTest(unittest.TestCase):
         router = Router(container)
         router.add_route('GET', '/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['GET/home'], {'method': 'GET', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['GET/home'], {
+            'method': 'GET',
+            'uri': '/home',
+            'action': {
+                'uses':'test@test'
+            }
+        })
 
     def test_add_route_trim_slashes(self):
         container = Container()
@@ -30,57 +36,111 @@ class RouterTest(unittest.TestCase):
         router = Router(container)
         router.add_route(['GET', 'POST'],'/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['GET/home'], {'method': 'GET', 'uri': '/home', 'action': 'test@test'})
-        self.assertEqual(routes['POST/home'], {'method': 'POST', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['GET/home'], {
+            'method': 'GET',
+            'uri': '/home',
+            'action': {
+                'uses': 'test@test'
+            }
+        })
+        self.assertEqual(routes['POST/home'], {
+            'method': 'POST',
+            'uri': '/home',
+            'action': {
+                'uses': 'test@test'
+            }
+        })
 
     def test_add_route_with_head_verb(self):
         container = Container()
         router = Router(container)
         router.head('/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['HEAD/home'], {'method': 'HEAD', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['HEAD/home'], {
+            'method': 'HEAD',
+            'uri': '/home',
+            'action': {
+                'uses': 'test@test'
+            }
+        })
 
     def test_add_route_with_get_verb(self):
         container = Container()
         router = Router(container)
         router.get('/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['GET/home'], {'method': 'GET', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['GET/home'], {
+            'method': 'GET',
+            'uri': '/home',
+            'action': {
+                'uses': 'test@test'
+            }
+        })
 
     def test_add_route_with_post_verb(self):
         container = Container()
         router = Router(container)
         router.post('/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['POST/home'], {'method': 'POST', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['POST/home'], {
+            'method': 'POST',
+            'uri': '/home',
+            'action': {
+                'uses':'test@test'
+            }
+        })
 
     def test_add_route_with_put_verb(self):
         container = Container()
         router = Router(container)
         router.put('/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['PUT/home'], {'method': 'PUT', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['PUT/home'], {
+            'method': 'PUT',
+            'uri': '/home',
+            'action': {
+                'uses':'test@test',
+            }
+        })
 
     def test_add_route_with_patch_verb(self):
         container = Container()
         router = Router(container)
         router.patch('/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['PATCH/home'], {'method': 'PATCH', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['PATCH/home'], {
+            'method': 'PATCH',
+            'uri': '/home',
+            'action': {
+                'uses':'test@test'
+            }
+        })
 
     def test_add_route_with_delete_verb(self):
         container = Container()
         router = Router(container)
         router.delete('/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['DELETE/home'], {'method': 'DELETE', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['DELETE/home'], {
+            'method': 'DELETE',
+            'uri': '/home',
+            'action': {
+                'uses': 'test@test'
+            }
+        })
 
     def test_add_route_with_options_verb(self):
         container = Container()
         router = Router(container)
         router.options('/home', 'test@test')
         routes = router.get_routes()
-        self.assertEqual(routes['OPTIONS/home'], {'method': 'OPTIONS', 'uri': '/home', 'action': 'test@test'})
+        self.assertEqual(routes['OPTIONS/home'], {
+            'method': 'OPTIONS',
+            'uri': '/home',
+            'action': {
+                'uses': 'test@test'
+            }
+        })
 
     def test_add_group_routes(self):
         container = Container()
@@ -95,13 +155,61 @@ class RouterTest(unittest.TestCase):
             'prefix': 'admin'
         })
         def group(_router):
-            _router.get('/home', 'action@home')
-            _router.post('/about', 'action@about')
+            _router.get('/home', {
+                'as' : 'home',
+                'uses': 'CMSController@home'
+            })
+            _router.post('/about', 'CMSController@about')
+            @router.group({
+                "middleware": "auth",
+                'before': 'auth',
+                'namespace': 'users.',
+                'domain': '{account}.myapp.com',
+                'prefix': 'users'
+            })
+            def _group(_router):
+                _router.get('/', {
+                    'as': 'users',
+                    'uses': 'UserController@index',
+                    'middleware': 'permissions'
+                })
+                _router.get('/{id}', 'UserController@show')
 
         routes = router.get_routes()
-        print(router.group_stack)
-        self.assertEqual(routes['GET/home'], {'method': 'GET', 'uri': '/home', 'action': 'action@home'})
-        self.assertEqual(routes['POST/about'], {'method': 'POST', 'uri': '/about', 'action': 'action@about'})
+        self.assertEqual(routes['GET/admin/home'], {
+            'method': 'GET',
+            'uri': '/admin/home',
+            'action': {
+                'as': 'home',
+                'uses':'admin.CMSController@home',
+                'middleware': ['auth']
+            }
+        })
+        self.assertEqual(routes['POST/admin/about'], {
+            'method': 'POST',
+            'uri': '/admin/about',
+            'action': {
+                'uses': 'admin.CMSController@about',
+                'middleware': ['auth']
+            }
+        })
+        self.assertEqual(routes['GET/admin/users'], {
+            'method': 'GET',
+            'uri': '/admin/users',
+            'action': {
+                'as': 'users',
+                'uses': 'admin.users.UserController@index',
+                'middleware': ['auth', 'permissions']
+            }
+        })
+        self.assertEqual(routes['GET/admin/users/{id}'], {
+            'method': 'GET',
+            'uri': '/admin/users/{id}',
+            'action': {
+                'uses': 'admin.users.UserController@show',
+                'middleware': ['auth']
+            }
+        })
 
 if __name__ == '__main__':
     unittest.main()
